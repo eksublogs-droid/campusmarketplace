caf9168653f642394bca9261c38e6183d9bde8d6
eksublogs-droid <eksublogs@gmail.com>
2026-05-26 01:57:59 +0100

    unzipgit

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  telegramId: { type: Number, required: true, unique: true },
+  firstName: { type: String, default: '' },
+  lastName: { type: String, default: '' },
+  username: { type: String, default: '' },
+  gmail: { type: String, default: '' },
+  gmailSubmitted: { type: Boolean, default: false },
+  verified: { type: Boolean, default: false },
+  verifyCode: { type: String, default: '' },
+  registeredAt: { type: Date, default: Date.now },
+  lastSeen: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
