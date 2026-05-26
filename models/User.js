b0ee77355fe69d4e2a50631edfda8d5bafddf40d
eksublogs-droid <eksublogs@gmail.com>
2026-05-26 13:00:11 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -14,4 +14,4 @@ const userSchema = new mongoose.Schema({
   lastSeen: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
